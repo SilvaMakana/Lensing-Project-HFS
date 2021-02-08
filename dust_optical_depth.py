35adc7570d3c888a290c4434c97824ac1e6f4bbf
Makana Silva <silva.179@osu.edu>
2021-02-08 02:08:38 -0500

    Mean optical depth of the Universe due to dust from galaxies

--- dust_optical_depth.py
+++ dust_optical_depth.py
@@ -1,4 +1,12 @@
 ##Building Dust Model from arXiv:0902.4240v1
+import sys
+import numpy as np
+from scipy import optimize
+import matplotlib.pyplot as plt
+import pandas as pd
+from math import e
+from scipy.interpolate import interp2d, interp1d,InterpolatedUnivariateSpline,RectBivariateSpline
+from global_variables import * 
 wavelength_V = 5.50*10**(-7) #in units of meters
 r_virial = 0.110 #in units of h^-1 Mpc
 numberdensity_galaxy = 0.037 #comoving number density of galaxies in units of h^3 Mpc^-3
@@ -23,9 +31,10 @@ def tau_g(z):
 def tau_meandust(wavelength,n,z_ini,z_f):
 	sigma_galaxy = np.pi * r_virial**2
 	tau_dust = 0
-	for i in range (n):
-		delta_z = (z_f-z_ini)/n
-		zi = (z_ini + i*delta_z)
-		zmid = 1/2*(zi + z_ini + (i+1)*delta_z)
-		tau_dust += sigma_galaxy*numberdensity_galaxy*tau_g(zmid)*(1+zmid)**(2)*d_h/np.sqrt(Omega_r*(1+zmid)**4 + Omega_m*(1+zmid)**3 + Omega_k*(1+zmid)**2 + Omega_L) * delta_z
+	delta_z = (z_f-z_ini)/n
+	#for i in range (n):
+		#zi = (z_ini + i*delta_z)
+		#zmid = 1/2*(zi + z_ini + (i+1)*delta_z)
+	zmid = np.linspace(0.5*delta_z,(n-1/2)*delta_z,n)
+	tau_dust = np.sum(sigma_galaxy*numberdensity_galaxy*tau_g(zmid)*(1+zmid)**(2)*d_h/np.sqrt(Omega_r*(1+zmid)**4 + Omega_m*(1+zmid)**3 + Omega_k*(1+zmid)**2 + Omega_L)) * delta_z
 	return (tau_dust)
