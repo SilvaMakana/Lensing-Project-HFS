84a5ea8eaff21120654d50352933260d37b266b4
Makana Silva <silva.179@osu.edu>
2021-03-04 21:12:02 -0500

    trying to fix shape of integrand

--- halo_model_dust_bispec.py
+++ halo_model_dust_bispec.py
@@ -56,9 +56,13 @@ def alpha_parameter(stellarstuff,parameter_cutoff):
 	Menard_dust_value = 4.109 * 10**7 #mass of dust in a halo with halo mass Menard_halo_mass
 	return(np.exp(Menard_halo_mass/parameter_cutoff + np.log(Menard_dust_value/M_dust_optimistic(Menard_halo_mass,stellarstuff))))
 
-def dust_model_1(z,M_halo,parameter_cutoff):
+def dust_model_1(z,M_halo):
+	cut_off_value = 13102297747878.25 #value of parameter_cutoff in h^-1 M_S
 	stellar_info = parameters_stellarMvshaloM(z)
-	return(alpha_parameter(stellar_info,parameter_cutoff) * M_dust_optimistic(M_halo,stellar_info) *np.exp(-M_halo/parameter_cutoff))
+	#stellar_info_measurement = parameters_stellarMvshaloM(0.36)
+	#alpha = alpha_parameter(stellar_info_measurement,cut_off_value)
+	alpha = 0.34281932422102807 #numerical value of alpha_parameter(stellar_info_measurement,cut_off_value)
+	return(alpha * M_dust_optimistic(M_halo,stellar_info) * np.exp(-M_halo/cut_off_value))
 
 #Normalization constant that is defined by the dust mass model in dust_bias_func in a halo of mass M_halo
 def normalization_rho(z,M_halo):
@@ -66,7 +70,7 @@ def normalization_rho(z,M_halo):
 	delta_r_halo = r_halo_virial(M_halo)/n
 	r_halo_mid = np.linspace(0.5*delta_r_halo,(n-1/2)*delta_r_halo,n)
 	r_integral = np.sum((r_halo_mid)**(-1.84) * r_halo_mid**2) * delta_r_halo
-	return(dust_model_1(z,M_halo,11829265763208.602)/(4*np.pi * r_integral))
+	return(dust_model_1(z,M_halo)/(4*np.pi * r_integral))
 
 #Dust density as a function of M_halo and size of the halo (r_halo)
 def rho_dust(z,r_halo,M_halo):
@@ -82,7 +86,7 @@ def u_dust_halo_parameter(z,k,M):
 	delta_r_dust = r_halo_virial(M)/n
 	r_dust_mid = np.linspace(0.5*delta_r_dust,(n-1/2)*delta_r_dust,n)
 	dust_parameter = np.sum(r_dust_mid**2 * np.sin(k*r_dust_mid)/(k*r_dust_mid) * rho_dust(z,r_dust_mid,M)) * delta_r_dust
-	return(1/dust_model_1(z,M,11829265763208.602) * 4*np.pi * dust_parameter)
+	return(1/dust_model_1(z,M) * 4*np.pi * dust_parameter)
 
 
 #def rho_bar_dust(z):
@@ -99,7 +103,7 @@ def rho_bar_dust(z):
 	delta_M_halo = M_halo_min * (M_halo_max/M_halo_min)**(np.linspace(0,n_halo_integral_step-1,n_halo_integral_step)/n_halo_integral_step)*epsilon
 	M_halo_mid = M_halo_min * (M_halo_max/M_halo_min)**(np.linspace(0,n_halo_integral_step-1,n_halo_integral_step)/n_halo_integral_step) * (1 + epsilon/2)
 	#M_halo_mid = np.linspace(0,n_halo_integration_step-1,n_halo_integration_step)
-	rhobardust = np.sum(dust_model_1(z,M_halo_mid,11829265763208.602) * halo_distribution_function(z,M_halo_mid) * delta_M_halo)
+	rhobardust = np.sum(dust_model_1(z,M_halo_mid) * halo_distribution_function(z,M_halo_mid) * delta_M_halo)
 	return(rhobardust)
 
 #Building the I-integrals that will be used to build the single, double, and triple halo bispectrum contributors but with dust density profiles for k3
@@ -112,11 +116,38 @@ def I_03_dust(z,myTriangle,halo_stuff):
 		#M_halo_i = delta_M_halo*i
 		#M_halo_mid = 1/2*(M_halo_i + (i+1)*delta_M_halo)
 		M_halo_mid = M_halo_min * (M_halo_max/M_halo_min)**(i/n_halo_integral_step) * (1 + epsilon/2)
-		I03dust += (M_halo_mid/rho_background_matter)**2 * (dust_model_1(z,M_halo_mid,11829265763208.602)) * halo_stuff.dn_dm_array[i] * y_halo_parameter2(myTriangle.k1,M_halo_mid,halo_stuff,i) * y_halo_parameter2(myTriangle.k2,M_halo_mid,halo_stuff,i) * u_dust_halo_parameter(z,myTriangle.k3,M_halo_mid) * delta_M_halo
+		I03dust += (M_halo_mid/rho_background_matter)**2 * (dust_model_1(z,M_halo_mid)) * halo_stuff.dn_dm_array[i] * y_halo_parameter2(myTriangle.k1,M_halo_mid,halo_stuff,i) * y_halo_parameter2(myTriangle.k2,M_halo_mid,halo_stuff,i) * u_dust_halo_parameter(z,myTriangle.k3,M_halo_mid) * delta_M_halo
 	return(I03dust)
 
-
 def I_12_dust(z,myTriangle,halo_stuff,index):
+	I12dust = 0
+	epsilon = (M_halo_max/M_halo_min)**(1/n_halo_integral_step) - 1
+	delta_M_halo = M_halo_min* (M_halo_max/M_halo_min)**(np.linspace(0,n_halo_integral_step-1,n_halo_integral_step)/n_halo_integral_step)*epsilon
+	M_halo_mid = M_halo_min * (M_halo_max/M_halo_min)**(np.linspace(0,n_halo_integral_step-1,n_halo_integral_step)/n_halo_integral_step) * (1 + epsilon/2)
+	#delta_M_halo = (10**16 - 10**8)/n_halo_integral_step
+	#M_halo_i = delta_M_halo*i
+	#M_halo_mid = 1/2*(M_halo_i + (i+1)*delta_M_halo)
+	k1 = myTriangle.k1; k2 = myTriangle.k2
+	spacing1 = np.linspace(0,n_halo_integral_step-1,n_halo_integral_step)
+	spacing2 = spacing1.astype(int)
+	#print(np.shape(halo_stuff.dn_dm_array[i],halo_stuff.bias1_array[i],delta_M_halo,M_halo_mid,y_halo_parameter2(k1,M_halo_mid,halo_stuff,i)))
+	profile_func1 = (M_halo_mid/rho_background_matter) * y_halo_parameter2(k1,M_halo_mid,halo_stuff,spacing2).T
+	profile_func2 = (M_halo_mid/rho_background_matter)*y_halo_parameter2(k2,M_halo_mid,halo_stuff,spacing2)
+	if index==1:
+		k1 = myTriangle.k2; k2 = myTriangle.k3
+		profile_func1 = (M_halo_mid/rho_background_matter)*y_halo_parameter2(k1,M_halo_mid,halo_stuff,spacing2)
+		profile_func2 = (dust_model_1(z,M_halo_mid))*u_dust_halo_parameter(z,k2,M_halo_mid)
+
+	if index==2:
+		k1 = myTriangle.k3; k2 = myTriangle.k1
+		profile_func1 = (dust_model_1(z,M_halo_mid))*u_dust_halo_parameter(z,k1,M_halo_mid)
+		profile_func2 = (M_halo_mid/rho_background_matter)*y_halo_parameter2(k2,M_halo_mid,halo_stuff,spacing2)
+	I12dust = np.sum(halo_stuff.dn_dm_array[i] * halo_stuff.bias1_array[i] * profile_func1 * profile_func2 * delta_M_halo)
+		#print (I12)
+	return(I12dust)
+
+
+"""def I_12_dust(z,myTriangle,halo_stuff,index):
 	I12dust = 0
 	for i in range(n_halo_integral_step):
 		epsilon = (M_halo_max/M_halo_min)**(1/n_halo_integral_step) - 1
@@ -126,21 +157,21 @@ def I_12_dust(z,myTriangle,halo_stuff,index):
 		#M_halo_i = delta_M_halo*i
 		#M_halo_mid = 1/2*(M_halo_i + (i+1)*delta_M_halo)
 		k1 = myTriangle.k1; k2 = myTriangle.k2
-		profile_func1 = (M_halo_mid/rho_background_matter)*y_halo_parameter2(k1,M_halo_mid,halo_stuff,i)
+		#print(np.shape(halo_stuff.dn_dm_array[i],halo_stuff.bias1_array[i],delta_M_halo,M_halo_mid,y_halo_parameter2(k1,M_halo_mid,halo_stuff,i)))
+		profile_func1 = (M_halo_mid/rho_background_matter) * y_halo_parameter2(k1,M_halo_mid,halo_stuff,i)
 		profile_func2 = (M_halo_mid/rho_background_matter)*y_halo_parameter2(k2,M_halo_mid,halo_stuff,i)
-
 		if index==1:
 			k1 = myTriangle.k2; k2 = myTriangle.k3
 			profile_func1 = (M_halo_mid/rho_background_matter)*y_halo_parameter2(k1,M_halo_mid,halo_stuff,i)
-			profile_func2 = (dust_model_1(z,M_halo_mid,11829265763208.602))*u_dust_halo_parameter(z,k2,M_halo_mid)
+			profile_func2 = (dust_model_1(z,M_halo_mid))*u_dust_halo_parameter(z,k2,M_halo_mid)
 
 		if index==2:
 			k1 = myTriangle.k3; k2 = myTriangle.k1
-			profile_func1 = (dust_model_1(z,M_halo_mid,11829265763208.602))*u_dust_halo_parameter(z,k1,M_halo_mid)
+			profile_func1 = (dust_model_1(z,M_halo_mid))*u_dust_halo_parameter(z,k1,M_halo_mid)
 			profile_func2 = (M_halo_mid/rho_background_matter)*y_halo_parameter2(k2,M_halo_mid,halo_stuff,i)
 		I12dust = np.sum(halo_stuff.dn_dm_array[i] * halo_stuff.bias1_array[i] * profile_func1 * profile_func2 * delta_M_halo)
 		#print (I12)
-	return(I12dust)
+	return(I12dust)"""
 
 def I_11_dust(z,myTriangle,halo_stuff,index):
 	#bias_1 = 1  + (2*p_halo - 1)/critical_density_parameter
@@ -163,7 +194,7 @@ def I_11_dust(z,myTriangle,halo_stuff,index):
 		if index==2:
 			k1 = myTriangle.k3
 			profile_func = u_dust_halo_parameter(z,k1,M_halo_mid)
-			prefactor = dust_model_1(z,M_halo_mid,11829265763208.602)
+			prefactor = dust_model_1(z,M_halo_mid)
 		I11dust += prefactor * halo_stuff.dn_dm_array[i] * halo_stuff.bias1_array[i] * profile_func * delta_M_halo
 		#transformI11dust += prefactor * halo_stuff.dn_dm_array[i] * (bias_1 - halo_stuff.bias1_array[i] * profile_func) * delta_M_halo
 		#print (I11)
@@ -191,7 +222,7 @@ def I_21_dust(z,myTriangle,halo_stuff,index):
 		if index==2:
 			k1 = myTriangle.k3
 			profile_func = u_dust_halo_parameter(z,k1,M_halo_mid)
-			prefactor = dust_model_1(z,M_halo_mid,11829265763208.602)
+			prefactor = dust_model_1(z,M_halo_mid)
 		I21dust += prefactor * halo_stuff.dn_dm_array[i] * halo_stuff.bias2_array[i] * profile_func * delta_M_halo
 		#print (I21)
 	#return(bias_2 - transformI21dust)
