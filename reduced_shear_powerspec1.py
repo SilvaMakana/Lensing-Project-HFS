dbded4fbd4bd216d764d1c7675d5e2acd84e4022
Makana Silva <silva.179@osu.edu>
2021-03-08 22:39:25 -0500

    rearrange

--- reduced_shear_powerspec1.py
+++ reduced_shear_powerspec1.py
@@ -31,6 +31,7 @@ def reduced_shear1(z_ini,l_tripleprime_max,z_alpha,z_beta,l_mag,l_phi):
 		D_mid = distance(z_ini,zmid)
 		window_alpha = window_distance(D_mid,D_alpha)
 		window_beta = window_distance(D_mid,D_beta)
+		factor = 2*window_alpha * window_beta * sigma_galaxy*numberdensity_galaxy*tau_g(zmid)*(1+zmid)**(2)*d_h/np.sqrt(Omega_r*(1+zmid)**4 + Omega_m*(1+zmid)**3 + Omega_k*(1+zmid)**2 + Omega_L) * (9*Omega_m**2*d_h**(-4))/(4 *1/(1+zmid)**2)
 		#l_tripleprime magnitude integral from 0 to some max
 		delta_l_tripleprime = (l_tripleprime_max)/l_tripleprime_step
 		for j in range (l_tripleprime_step):
@@ -41,6 +42,6 @@ def reduced_shear1(z_ini,l_tripleprime_max,z_alpha,z_beta,l_mag,l_phi):
 			for k in range (phi_step):
 				phi_k = k*delta_phi
 				phi_mid = 1/2*(phi_k + (k+1)*delta_phi)
-				shear += 2*window_alpha * window_beta * sigma_galaxy*numberdensity_galaxy*tau_g(zmid)*(1+zmid)**(2)*d_h/np.sqrt(Omega_r*(1+zmid)**4 + Omega_m*(1+zmid)**3 + Omega_k*(1+zmid)**2 + Omega_L) * np.cos(2*l_phi - 2*phi_mid) * (9*Omega_m**2*d_h**(-4))/(4 *1/(1+zmid)**2) * total_halo_dust_bispectrum(zmid,kTriangle(l_mag/D_mid,l_tripleprime_mid/D_mid,l_phi - phi_mid))[0,0] * 1/(2*np.pi)**2 * delta_z * delta_phi * l_tripleprime_mid * delta_l_tripleprime
+				shear += factor * np.cos(2*l_phi - 2*phi_mid)  * total_halo_dust_bispectrum(zmid,kTriangle(l_mag/D_mid,l_tripleprime_mid/D_mid,l_phi - phi_mid))[0,0] * 1/(2*np.pi)**2 * delta_z * delta_phi * l_tripleprime_mid * delta_l_tripleprime
 				print(i,j,k,shear)
 	return (shear)
