6233316453a2857fc055685ba4384579b86be22f
Makana Silva <silva.179@osu.edu>
2021-03-08 21:54:40 -0500

    update

--- reduced_shear_powerspec1.py
+++ reduced_shear_powerspec1.py
@@ -1,4 +1,4 @@
-##Building reduced shear model with Matter Bispectrum model from perturbation theory
+##Building reduced shear power spectrum with Matter-Matter-Dust Bispectrum model
 import sys
 import numpy as np
 from scipy import optimize
@@ -12,12 +12,13 @@ from dust_optical_depth import *
 from perturb_matter_bispec import * 
 from CLASS_matter_powerspec import *
 from halo_model_bispec import *
+from halo_model_dust_bispec import *
 from global_variables import * 
 
-z_step = 8
-l_tripleprime_step = 16
-phi_step = 16
-def reduced_shear(z_ini,l_tripleprime_max,z_alpha,z_beta,l_mag,l_phi):
+z_step = 4
+l_tripleprime_step = 8
+phi_step = 8
+def reduced_shear1(z_ini,l_tripleprime_max,z_alpha,z_beta,l_mag,l_phi):
 	sigma_galaxy = np.pi * r_virial**2
 	shear = 0
 	D_alpha = distance(z_ini,z_alpha)
@@ -38,6 +39,6 @@ def reduced_shear(z_ini,l_tripleprime_max,z_alpha,z_beta,l_mag,l_phi):
 			for k in range (phi_step):
 				phi_k = k*delta_phi
 				phi_mid = 1/2*(phi_k + (k+1)*delta_phi)
-				shear += 2*window_distance(D_mid,D_alpha) * window_distance(D_mid,D_beta) * sigma_galaxy*numberdensity_galaxy*tau_g(zmid)*(1+zmid)**(2)*d_h/np.sqrt(Omega_r*(1+zmid)**4 + Omega_m*(1+zmid)**3 + Omega_k*(1+zmid)**2 + Omega_L) * np.cos(2*l_phi - 2*phi_mid) * (9*Omega_m**2*d_h**(-4))/(4 *1/(1+zmid)**2) * B_matterspec(zmid,kTriangle(l_mag/D_mid,l_tripleprime_mid/D_mid,l_phi - phi_mid)) * 1/(2*np.pi)**2 * delta_z * delta_phi * l_tripleprime_mid * delta_l_tripleprime
-				#print(i,j,k,shear)
+				shear += 2*window_distance(D_mid,D_alpha) * window_distance(D_mid,D_beta) * sigma_galaxy*numberdensity_galaxy*tau_g(zmid)*(1+zmid)**(2)*d_h/np.sqrt(Omega_r*(1+zmid)**4 + Omega_m*(1+zmid)**3 + Omega_k*(1+zmid)**2 + Omega_L) * np.cos(2*l_phi - 2*phi_mid) * (9*Omega_m**2*d_h**(-4))/(4 *1/(1+zmid)**2) * total_halo_dust_bispectrum(zmid,kTriangle(l_mag/D_mid,l_tripleprime_mid/D_mid,l_phi - phi_mid))[0,0] * 1/(2*np.pi)**2 * delta_z * delta_phi * l_tripleprime_mid * delta_l_tripleprime
+				print(i,j,k,shear)
 	return (shear)
